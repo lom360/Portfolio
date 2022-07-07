2fef18105389e0290dcfc04694f26e9866a1e26a
Lom Thin <sarom_thin@outlook.com>
2022-07-07 06:51:11 -0700

    nav buttons adjust color properly along with scroll

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,7 @@ const scrollToTop = document.querySelector("#toHead")
 const scrollToAbout = document.querySelector("#toAbout");
 const scrollToSkills = document.querySelector("#toSkills");
 const scrollToProjects = document.querySelector("#toProjects");
-const scrollToContact = document.querySelector("#Contact");
+const scrollToContact = document.querySelector("#toContact");
 const enterPortfolio = document.querySelector("#portfolio-btn button");
 
 scrollToAbout.addEventListener("click", function () {
@@ -33,6 +33,34 @@ toggleNav = false;
 window.onscroll = function() {
 	var currentScrollPos = window.pageYOffset;
 	var element = document.getElementById('navigation');
+	controlNavColors(currentScrollPos);
+	navBarToggle(currentScrollPos, element);
+}
+
+function controlNavColors(scrollPos) {
+	if(scrollPos >= 3908) {
+		$('.nav-link').removeClass('addNavColor');
+		$('#toContact').addClass('addNavColor');
+	}
+	else if(scrollPos >= 2908) {
+		$('.nav-link').removeClass('addNavColor');
+		$('#toProjects').addClass('addNavColor');
+	}
+	else if(scrollPos >= 1908) {
+		$('.nav-link').removeClass('addNavColor');
+		$('#toSkills').addClass('addNavColor');
+	}
+	else if(scrollPos >= 908) {
+		// $('#toAbout').css('color','#C1E51A')
+		$('.nav-link').removeClass('addNavColor');
+		$('#toAbout').addClass('addNavColor');
+	}
+	else {
+		$('.nav-link').removeClass('addNavColor');
+	}
+}
+
+function navBarToggle(currentScrollPos, element) {
 	if(currentScrollPos > 10 && toggleNav == false) {
 		element.classList.add('bg-dark');
 		$("#navigation").animate({'height':'53px'},0.0001);
