b133fff5a716acce312c93179e1b2e6d8050af3a
Lom Thin <sarom_thin@outlook.com>
2022-07-10 08:26:31 -0700

    Adjusted vh

--- public/js/script.js
+++ public/js/script.js
@@ -73,3 +73,8 @@ function navBarToggle(currentScrollPos, element) {
 		toggleNav = !toggleNav;
 	}
 }
+
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
