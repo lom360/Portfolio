f40bdccedd25df92c90c256e7cdfd95190cf8a49
Lom Thin <sarom_thin@outlook.com>
2022-07-13 09:21:45 -0700

    Improved color scheme and improved flex flow of skills section

--- public/js/script.js
+++ public/js/script.js
@@ -79,3 +79,6 @@ function navBarToggle(currentScrollPos, element) {
 let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+
+console.log(window.innerWidth);
