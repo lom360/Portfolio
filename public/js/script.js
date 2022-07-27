47b465b278210c6e2d3877ea93378aacc03d415a
Lom Thin <sarom_thin@outlook.com>
2022-07-26 21:27:34 -0700

    Hamburger function near completion

--- public/js/script.js
+++ public/js/script.js
@@ -5,6 +5,18 @@ const scrollToProjects = document.querySelector("#toProjects");
 const scrollToContact = document.querySelector("#toContact");
 const enterPortfolio = document.querySelector("#portfolio-btn button");
 
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+	hamburger.classList.toggle("active");
+	navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+	hamburger.classList.remove("active");
+	navMenu.classList.remove("active");
+}))
 // scrollToTop.addEventListener("click", function () {
 // 	$("html, body").animate({scrollTop: 0},1);
 // })
