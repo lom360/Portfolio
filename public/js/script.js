8aaacf27ce16822153b954c68544ad60e3eaf122
Lom Thin <sarom_thin@outlook.com>
2022-07-28 00:22:00 -0700

    Fixed bug where navbar toggle acted strangely with different combination of scroll positio and screen size

--- public/js/script.js
+++ public/js/script.js
@@ -43,31 +43,96 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
 
 
 toggleNav = false;
-window.onscroll = function() {
-	var currentScrollPos = window.pageYOffset;
+smallScreen = false;
+count = 0;
+s1 = false;
+
+function getCurrentPosition() {
+	return window.pageYOffset;
+}
+function getCurrentWidth() {
+	return window.innerWidth;
+}
+function checkSize() {
+	if(getCurrentWidth() <= 991 && smallScreen == false) {
+		smallScreen = true;
+		// count++;
+	}
+	else if(getCurrentWidth() > 991 && smallScreen == true) {
+		smallScreen = false;
+		// count = 0;
+	}
+}
+function repeatedActions() {
 	var element = document.getElementById('navigation');
-	controlNavColors(currentScrollPos);
-	navBarToggle(currentScrollPos, element);
+	navBarToggle(getCurrentPosition(), element);
 }
 
-function controlNavColors(scrollPos) {
+window.onload = function() {
+	checkSize();
+	repeatedActions();
+}
+function navBarToggle(currentScrollPos, element) { // function navBarToggle(currentScrollPos, element)
+	if(smallScreen ) {
+		addNavBar(element);
+	}
+	else {
+		if(currentScrollPos > 10 && toggleNav == false) {
+			addNavBar(element);
+			toggleNav = !toggleNav;
+		}
+		else if(currentScrollPos == 0 && toggleNav == false) {
+			removeNavBar(element);
+
+		}
+		else if(currentScrollPos == 0 && toggleNav == true) {
+			removeNavBar(element);
+			toggleNav = !toggleNav;
+		}
+	}
+}
+
+// window.onresize = function() {
+// 	checkSize();
+// 	if(smallScreen && count == 1) {
+// 		repeatedActions();
+// 	}
+// }
+
+var resizeId;
+window.addEventListener('resize', function() {
+	// checkSize();
+    clearTimeout(resizeId);
+    resizeId = setTimeout(doneResizing, 500);
+});
+
+function doneResizing(){
+	checkSize();
+	repeatedActions();
+}
+
+window.onscroll = function() {
+	controlNavColors(getCurrentPosition());
+	repeatedActions()
+}
+function controlNavColors(currentScrollPos) {
 	contactPos = $('#contact').offset().top - 53;
 	projectsPos = $('#projects').offset().top - 53;
 	skillsPos = $('#skills').offset().top - 53;
 	aboutPos = $('#about').offset().top - 53;
-	if(scrollPos >= contactPos) { // 3448
+	if(currentScrollPos >= contactPos) { // 3448
 		$('.nav-link').removeClass('addNavColor');
 		$('#toContact').addClass('addNavColor');
 	}
-	else if(scrollPos >= projectsPos) { // 2448
+	else if(currentScrollPos >= projectsPos) { // 2448
 		$('.nav-link').removeClass('addNavColor');
 		$('#toProjects').addClass('addNavColor');
 	}
-	else if(scrollPos >= skillsPos) { // 1448
+	else if(currentScrollPos >= skillsPos) { // 1448
 		$('.nav-link').removeClass('addNavColor');
 		$('#toSkills').addClass('addNavColor');
 	}
-	else if(scrollPos >= aboutPos) { // 908
+	else if(currentScrollPos >= aboutPos) { // 908
 		$('.nav-link').removeClass('addNavColor');
 		$('#toAbout').addClass('addNavColor');
 	}
@@ -76,22 +141,33 @@ function controlNavColors(scrollPos) {
 	}
 }
 
-function navBarToggle(currentScrollPos, element) {
-	if(currentScrollPos > 10 && toggleNav == false) {
-		element.classList.add('bg-dark');
-		$("#navigation").animate({'height':'53px'},0);
-		$("#brand").animate({'font-size':'25px'},200);
-		// $(".nav-menu").animate({'left':'326px'});
-		toggleNav = !toggleNav;
-	}
-	else if(currentScrollPos == 0 && toggleNav == true) {
-		element.classList.remove('bg-dark');
-		$("#navigation").animate({'height':'77px'},0);
-		$("#brand").animate({'font-size':'34px'},200);
-		// $(".nav-menu").animate({'left':'300px'});
-		toggleNav = !toggleNav;
-	}
+function addNavBar(element) {
+	element.classList.add('bg-dark');
+	$("#navigation").animate({'height':'53px'},0);
+	$("#brand").animate({'font-size':'25px'},200);
+	// toggleNav = !toggleNav;
 }
+
+function removeNavBar(element) {
+	element.classList.remove('bg-dark');
+	$("#navigation").animate({'height':'77px'},0);
+	$("#brand").animate({'font-size':'34px'},200);
+	// toggleNav = !toggleNav;
+}
+// function navBarToggle(element) {
+// 	if(toggleNav == false) {
+// 		element.classList.add('bg-dark');
+// 		$("#navigation").animate({'height':'53px'},0);
+// 		$("#brand").animate({'font-size':'25px'},200);
+// 		toggleNav = !toggleNav;
+// 	}
+// 	else if(toggleNav == true) {
+// 		element.classList.remove('bg-dark');
+// 		$("#navigation").animate({'height':'77px'},0);
+// 		$("#brand").animate({'font-size':'34px'},200);
+// 		toggleNav = !toggleNav;
+// 	}
+// }
 
 // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 let vh = window.innerHeight * 0.01;
